f141b1fe06aad4d5b676374d00f1c1cf010cf7d8
Nathan Jordan <natedagreat27274@gmail.com>
2014-02-27 18:34:49 -0800

    .

--- pyncs/pyncs.py
+++ pyncs/pyncs.py
@@ -1,6 +1,25 @@
 import requests
 import os
-import hashlib
+import json
+from urllib.parse import urlparse as urlparse
+
+
+class AuthenticationError(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return (self.value)
+
+
+class SimulationError(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return (self.value)
 
 
 class Simulator(object):
@@ -8,16 +27,94 @@ class Simulator(object):
     STATUS_RUNNING = 'STATUS_RUNNING'
     STATUS_IDLE = 'STATUS_IDLE'
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, username, password):
         self.host = host
         self.port = port
+        self.is_authenticated = False
+        url = urlparse(host)
+        if not url.port:
+            url.port = port
+        self.url = url
 
-    def get_status():
-        # TODO make this work
-        return Simulator.STATUS_IDLE
+    def authenticate(self):
+        self.url.path = "/login"
+        auth_payload = {
+            'username': self.username,
+            'password': self.password
+        }
+        r = requests.post(self.url.get_url(), data=json.dumps(auth_payload))
+        res = json.loads(r.text)
+        if 'token' not in res:
+            raise AuthenticationError("Authentication Failed")
+        else:
+            self.token = res['token']
 
-    def run(simulation):
+    def get_status(self):
+        self.url.path = "/sim"
+        r = requests.get(self.url.get_url())
+        res = json.loads(r.text)
+        if res['status'] is 'idle':
+            return Simulator.STATUS_IDLE
+        if res['status'] is 'running':
+            return Simulator.STATUS_RUNNING
+
+    def run(self, simulation):
+        # recurse through the model and build the simulation json object
+        entity_dicts = generate_entity_dicts(simulation.model)
+        # set the username
+        entity_dicts['meta'] = {'username': self.username}
+        # dump the dictionary to a json string
+        sim_string = json.dumps(entity_dicts)
+        # set the correct url path
+        self.url.path = '/sim'
+        # add the auth token to the request headers
+        headers = {'token': self.token}
+        # send the sim request
+        r = requests.post(self.url.get_url(), data=sim_string, headers=headers)
+        # if its not successful raise an exception
+        if r.status_code is not 200:
+            raise SimulationError(r.json()['message'])
+        # otherwise return info about the simulation from ncsdaemon
+        else:
+            return r.json()
+
+
+def generate_entity_dicts(self, model):
+    entity_dicts = {
+        'neuron_list': {},
+        'channel_list': {},
+        'report_list': {},
+        'stimuli_list': {},
+        'synapse_list': {},
+        'group_list': {},
+        'neuron_alias_list': {},
+        'synapse_alias_list': {}
+    }
+    subgroup_result_list = []
+    # check the current
+    if model._id not in entity_dicts['group_list']:
+        entity_dicts['group_list'][model['_id']] = model
+    else:
+        raise SimulationError("Recursive groups detected!")
+        return
+    # check/add neuron types and channels
+    for neuron_group in model.neuron_groups:
+        # if the neuron isn't in the neuron list yet
+        if neuron_group['neuron']._id not in entity_dicts['neuron_list']:
+            # add it to the list
+            entity_dicts['neuron_list'][neuron_group['neuron']._id] = neuron_group['neuron']
+            # check all of its channels
+            for channel in neuron_group['neuron']['channels']:
+                # if the channel isn't in the list, add it
+                if channel._id not in entity_dicts['channel_list']:
+                    entity_dicts['channel_list'][channel._id] = channel
+    for alias in model.neuron_aliases:
+        # TODO
         pass
+
+    # recursively traverse the subgroups of the model
+    for subgroup in model.subgroups:
+        subgroup_result_list.append(generate_entity_dicts(subgroup))
 
 
 class Simulation(object):
@@ -82,13 +179,6 @@ class Uniform(Entity):
     def __init__(self, min, max):
         self.min = min
         self.max = max
-
-
-class Neuron(Entity):
-    ''' Class for the neuron entity '''
-
-    def __init__(self, neuron_type):
-        self.neuron_type = neuron_type
 
 
 class Channel(Entity):
@@ -220,3 +310,76 @@ class Report(Entity):
         self.frequency = frequency
         self.time_start = time_start
         self.time_end = time_end
+
+
+class Neuron(Entity):
+
+    IZH_NEURON = 'IZH_NEURON'
+    NCS_NEURON = 'NCS_NEURON'
+    HH_NEURON = 'HH_NEURON'
+
+    def __init__(self, neuron_type):
+        Entity.__init__(self, entity_type=Entity.NEURON)
+        self.neuron_type = neuron_type
+
+
+class IzhNeuron(Neuron):
+
+    def __init__(self, a, b, c, d, u, v, threshold):
+        Neuron.__init__(self, Neuron.IZH_NEURON)
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.u = u
+        self.v = v
+        self.threshold = threshold
+
+
+class NCSNeuron(Neuron):
+
+    def __init__(self, threshold, spikeshape, resting_potential, calcium,
+                 calcium_spike_increment, tau_calcium,
+                 leak_reversal_potential, leak_conductance, tau_membrane,
+                 r_membrane, channels):
+        Neuron.__init__(self, Neuron.NCS_NEURON)
+        self.threshold = threshold
+        self.spikeshape = spikeshape
+        self.resting_potential = resting_potential
+        self.calcium = calcium
+        self.calcium_spike_increment = calcium_spike_increment
+        self.tau_calcium = tau_calcium
+        self.leak_reversal_potential = leak_reversal_potential
+        self.leak_conductance = leak_conductance
+        self.tau_membrane = tau_membrane
+        self.r_membrane = r_membrane
+        self.channels = channels
+
+
+class HHNeuron(Neuron):
+
+    def __init__(self, resting_potential, threshold, capacitance, channels):
+        Neuron.__init__(self, Neuron.HH_NEURON)
+        self.resting_potential = resting_potential
+        self.threshold = threshold
+        self.capacitance = capacitance
+        self.channels = channels
+
+
+class GroupType(Entity):
+
+    def __init__(self, geometry, subgroups, neuron_groups, neuron_aliases,
+                 synaptic_aliases, connections):
+        Entity.__init__(self, entity_type=Entity.GROUP)
+        self.geometry = geometry
+        self.subgroups = subgroups
+        self.neuron_groups = neuron_groups
+        self.neuron_aliases = neuron_aliases
+        self.synaptic_aliases = synaptic_aliases
+        self.connections = connections
+
+
+class GroupInstance(object):
+
+    def __init__(self, group_type):
+        pass
